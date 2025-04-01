d4d42776d631a0a71677c5d13c99d63e0ff6bc99
Petko <petkocholashki@gmail.com>
2025-04-01 17:51:36 +0300

    step6 - extend service to all CRUD operations

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Book } from '../models/book';
 
 @Injectable({
@@ -14,5 +14,55 @@ export class BookService {
 
   getBooks(): Observable<Book[]> {
     return this.http.get<Book[]>(this.apiUrl);
+  }
+
+  addBook(book: Book): Observable<boolean> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const bookAsJson = JSON.stringify(book);
+    return this.http.post<Book>(this.apiUrl, bookAsJson, { headers }).pipe(
+      map(response => {
+        return true;
+      }),
+      catchError(error => {
+        console.log(error);
+        return of(false);
+      })
+    );
+  }
+
+
+  getBookById(bookId: string): Observable<Book> {
+    return this.http.get<Book>(`${this.apiUrl}/${bookId}`);
+  }
+
+
+  putBook(book: Book) {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const bookAsJson = JSON.stringify(book);
+    this.http.put<Book>(`${this.apiUrl}/${book.id}`, bookAsJson, { headers }).subscribe({
+      next: (response) => {
+        console.log('Book put successfully:', response);
+      },
+      error: (error) => {
+        console.error('Error creating book:', error);
+      },
+      complete: () => {
+        console.log("compl");
+      }
+    });
+  }
+
+  deleteBook(bookId: string | undefined) {
+    this.http.delete<Book>(`${this.apiUrl}/${bookId}`,).subscribe({
+      next: (response) => {
+        console.log('Book deleted successfully:', response);
+      },
+      error: (error) => {
+        console.error('Error creadeleting book:', error);
+      },
+      complete: () => {
+        console.log("compl");
+      }
+    });
   }
 }
